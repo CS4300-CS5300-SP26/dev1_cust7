59d7e035ef5cbf37a35d39626e85db23d4ca8a5a
Roman Joska <rjoska@uccs.edu>
2026-03-09 19:09:49 +0000

    Got an inital test working for testing

--- cookBot/home/tests.py
+++ cookBot/home/tests.py
@@ -1,3 +1,14 @@
+import pytest
 from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+# https://docs.djangoproject.com/en/6.0/topics/testing/overview/ Reference as needed
+# Model tests need to be made
+
+# API tests are being made
+class TestAPI(APITestCase):
+    def test_index_page_200(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
