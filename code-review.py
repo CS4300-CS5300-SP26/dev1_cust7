dfdceab1ab2af396505b1340cc4f63fc8585df33
Ethan Koehn <135087029+ejkoehn12@users.noreply.github.com>
2026-03-10 13:10:19 -0600

    Create code-review.py

---
+++ code-review.py
@@ -0,0 +1,32 @@
+import os
+import openai
+from openai import OpenAI
+
+#Pulling OpenAI API Key
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+#Setting API Key inside the openai_client object
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
+#Getting file content from diff.txt
+with open("difference.txt","r") as diff_file:
+    diff = diff_file.read()
+
+chat_response = ""
+#Response object by querying ChatGPT
+try:
+    response = openai_client.chat.completions.create(
+        model="gpt-5.1-codex-mini",
+        messages=[
+            {"role": "system", "content": "You are a senior software engineer tasked with performing code reviews for a web based django project. Provide concise and actionable feedback."},
+            {"role":"user", "content": f"Provide concise and actionable feedback for this code, make sure to mention the file name and line number for each suggestion. Here is the pull request diff:\n{diff}"}
+        ]
+
+    )
+    chat_response = response.choices[0].message.content
+except openai.RateLimitError:
+    chat_response = "Querying ChatGPT Failed, AI Code Review Unavailable"
+
+#Saving response into the feedback.txt file
+with open("feedback.txt","w") as feedback_file:
+    feedback_file.write(chat_response)
