891df67545810c96c89cf99d52a4a4d56f8cb30a
Ethan Koehn <135087029+ejkoehn12@users.noreply.github.com>
2026-04-02 14:10:52 -0600

    Update cookBot/manage.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cookBot/manage.py
+++ cookBot/manage.py
@@ -2,9 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import re
-import json
-import math
 
 def test():
     print(  "hello world"  )  # whitespace fix
