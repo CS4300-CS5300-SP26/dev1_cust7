5726a73f84bc5a366546ff5b041a3ea5a88e9e9a
Glewis3 <glewisuccs@gmail.com>
2026-04-24 23:48:15 -0600

    added meal streak feature with reset option

--- cookBot/home/tests/test_meal_plan_history.py
+++ cookBot/home/tests/test_meal_plan_history.py
@@ -58,6 +58,6 @@ class MealPlanHistoryViewTests(TestCase):
             )
 
         self.client.login(username="testuser", password="password123")
-        with self.assertNumQueries(4):
+        with self.assertNumQueries(6):
             response = self.client.get(reverse("meal_plan_history"))
         self.assertEqual(response.status_code, 200)
